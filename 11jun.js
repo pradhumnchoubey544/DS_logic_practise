8a6649d86bf86b25c5cbced8136aa55975699b6b
pradhumnchoubey544 <125875243+pradhumnchoubey544@users.noreply.github.com>
2024-06-11 19:23:46 +0530

    Update 11jun.js

--- 11jun.js
+++ 11jun.js
@@ -239,6 +239,29 @@ function greet(name) {
 
 
 // #How to implement Bubble sort in JavaScript?
+function bubbleSort(arr) {
+  const n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    let swapped = false;
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        swapped = true;
+      }
+    }
+    if (!swapped) {
+      break;
+    }
+  }
+  return arr;
+}
+const numbers1 = [5, 1, 4, 2, 8];
+const sortedNumbers = bubbleSort(numbers1);
+console.log(sortedNumbers); // Op: [1, 2, 4, 5, 8]
+
+
 
 
 
