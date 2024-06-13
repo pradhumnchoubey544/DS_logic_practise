b4b16af09abaac2cd69045ba6066079410341174
pradhumnchoubey544 <125875243+pradhumnchoubey544@users.noreply.github.com>
2024-06-13 14:44:28 +0530

    add 13 code

---
+++ 13jun.js
@@ -0,0 +1,8 @@
+// promises video of akshay saini  3,4,5,6 
+
+
+
+const url = 'https://dummy.restapiexample.com/api/v1/employees';
+const data = fetch(url);
+console.log(data);
+setTimeout(()=>{console.log(data)},5000);
