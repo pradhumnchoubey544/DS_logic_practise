ca5522f5888fbb1017a0242ed030860a2e9f7255
pradhumnchoubey544 <125875243+pradhumnchoubey544@users.noreply.github.com>
2024-06-24 08:19:57 +0530

    add this code

--- 21jun.js
+++ 21jun.js
@@ -22,4 +22,27 @@ if(!(undefined > 100)){console.log("hello")}
 
 
 
+// case o/p hear setInterval will increment c by +1 with in 200ms and after 4000ms setinterval will execute clearInterva.
+// let c=0;
+// let z = setInterval(() => {
+// 	console.log(c++)
+// },200)
+// setTimeout(() => {
+// 	clearInterval(z)
+// },4000)
+
+
+// case o/p
+let a = true;
+//it will print hello..... serise till 2000 by will loop and after 2000 setTimeout will assine a = false so it stop
+setTimeout(() => {
+	a = false;
+},2000)
+
+while(a){
+	console.log('Hello')
+}
+
+
+
 
