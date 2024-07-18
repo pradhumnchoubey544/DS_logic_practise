dbf5e2c1a067b24dfaf582f61a76259a0a152ff7
Pradhumn1999 <pradhumn.choubey@nibavlifts.com>
2024-07-18 14:56:16 +0530

    add this code

---
+++ 16jul.js
@@ -0,0 +1,10 @@
+// gasss the output //
+
+let x = [];
+let y = [];
+console.log(x === y);
+console.log(x == y);
+// op false for both becouse in object and array both will compare referance not value.
+
+
+
