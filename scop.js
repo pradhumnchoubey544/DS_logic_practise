d9f771fca5801e54c108a1f007755c87ac572dde
pradhumnchoubey544 <125875243+pradhumnchoubey544@users.noreply.github.com>
2024-11-29 08:05:14 +0530

    this is final commit

---
+++ scop.js
@@ -0,0 +1,14 @@
+const name = 'rohon';
+const age = 12;
+
+function f1()
+{
+    const k = 10;
+    const j = 20;
+    console.log(k+j);
+}
+f1();
+console.log("PROGRAM END");
+//console.log(k+j);
+
+
